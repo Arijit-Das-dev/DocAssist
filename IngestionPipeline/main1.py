1260687775c656117b5983b2f65690622ee230f7
Arijit Das <arijitdas.das99@gmail.com>
2026-02-07 21:16:23 +0530

    First commit

---
+++ IngestionPipeline/main1.py
@@ -0,0 +1,89 @@
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_text_splitters import CharacterTextSplitter
+from transformers import AutoTokenizer
+import os
+
+class IngestionPipelineModel:
+
+    def load_documents(self, file_path="Docs"):
+
+        if not os.path.exists(file_path):
+
+            raise FileNotFoundError(f"___{file_path} IS MISSING___")
+
+        # loading the Folders + files
+        loader = DirectoryLoader(
+
+            path=file_path,
+            glob="**/*.pdf",
+            loader_cls=PyPDFLoader
+        )
+
+        print("___FILES LOADED SUCCESSFULLY___")
+
+        # Loading the folders and files
+        documents = loader.load()
+
+        if len(documents) == 0:
+
+            raise FileNotFoundError("___FOLDER CONTAINS NOTHING___")
+        
+        for i, doc in enumerate(documents):
+
+            print("="*100)
+            print(f"\nFile no. {i+1}")
+            print(f" \nSource: {doc.metadata['source']}") # File location/Name
+            print(f" \ncontent length: {len(doc.page_content)} characters") # total characters inside the text file
+            print(f" \ncontent preview: {doc.page_content[:100]}...") # Printing only the first 100 characters from the file
+            print(f" \nmetadata: {doc.metadata}")
+
+        return documents
+    
+    def text_to_chunks(self, document, chunk_size = 800, chunk_overlap = 0):
+
+        print("____GETTING READY OF CHUNKS____")
+
+        # for token count
+        tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+
+        # Splitting All the texts
+        text_splitter = CharacterTextSplitter(
+
+            chunk_size=chunk_size, # splitted upto 800 chars 
+            chunk_overlap=chunk_overlap 
+        )
+
+        # We are assigning all the splitted texts inside each chunks
+        chunks = text_splitter.split_documents(document)
+
+        if chunks:
+
+            for i, chunk in enumerate(chunks):
+
+                print("="*100)
+                print(f" \nChunk : {i+1}")
+                print(f" \nSource : {chunk.metadata['source']}") # Location 
+                tokens = tokenizer.encode(chunk.page_content)
+                num_tokens = len(tokens)
+                print(f"Tokens in chunk {i+1} : {num_tokens}") # Tokens in each chunk
+                print(f" \nContent:")
+                print(f" \n{chunk.page_content}") # Chunk Contents
+
+        return chunks
+
+    def create_and_persist_chroma_db(self, chunks, db_path="db/ChromaDB"):
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+        vector_store = Chroma.from_documents(
+            documents=chunks,
+            embedding=embeddings,
+            persist_directory=db_path,
+            collection_metadata={"hnsw:space": "cosine"}
+        )
+
+        print("____SUCCESSFULL_____")
+        return vector_store
---
+++ chat/ChatUI.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import os
+from IngestionPipeline.main1 import IngestionPipelineModel
+
+st.title("DocAssist",
+         width="stretch",
+         text_alignment="center"
+         )
+st.divider()
+
+
+st.sidebar.title("DocAssist")
+st.sidebar.caption("✔ Start PDF analysis")
+st.sidebar.caption("✔ Prepare document for search")
+st.sidebar.caption("✔ Index document into vector store")
+st.sidebar.caption("✔ Process PDF content")
+st.sidebar.caption("✔ Build knowledge from PDF")
+
+st.sidebar.divider()
+
+pdf_doc = st.sidebar.file_uploader(
+
+    label="upload pdf below",
+    type=["pdf"],
+    help="Only PDF files are supported. Max size depends on your system"
+)
+
+st.sidebar.divider()
+
+if pdf_doc:
+
+    file_path = os.path.join("Docs", pdf_doc.name)
+
+    if os.path.exists(file_path):
+        pass
+    else:
+        with open(file_path, "wb") as f:
+            f.write(pdf_doc.getbuffer())
+
+        st.sidebar.success(f"File saved successfully")
+
+def main():
+
+    print("\n____________Main Function____________\n")
+    ingest = IngestionPipelineModel()
+
+    document = ingest.load_documents(file_path="Docs")
+
+    splitted_chunks = ingest.text_to_chunks(document)
+
+    vector_db = ingest.create_and_persist_chroma_db(splitted_chunks)
+
+    print(vector_db)
+
+if st.sidebar.button("Press"):main()
